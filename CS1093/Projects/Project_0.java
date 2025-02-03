d2e4f6bc8e3273a548fc2af570d4081264336f51
John <55373552+PineappleHippy@users.noreply.github.com>
2025-02-02 22:08:29 -0600

    Create Project_0.java
    Creating the first project file.

---
+++ CS1093/Projects/Project_0.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public static class GroceryList {
+
+  public static void main(String[] args){
+    System.out.println("Hello World!\nWelcome to my Project 0 for my 2nd semester in UTSA.");
+  }
+}
