e8a21a94f3a19945125c8e4105e462866f146fe0
John <55373552+PineappleHippy@users.noreply.github.com>
2025-02-02 22:49:58 -0600

    Good Update Perhaps?

--- CS1093/Projects/Project_0.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-
-public static class GroceryList {
-
-  public static void main(String[] args){
-    System.out.println("Hello World!\nWelcome to my Project 0 for my 2nd semester in UTSA.");
-  }
-}
